510b46d51ebec5796de261c13286f3ff3e068d0f
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-05-13 13:57:20 +0200

    [add] migrate

--- database/migrations/2024_05_13_112929_create_trains_table.php
+++ database/migrations/2024_05_13_112929_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 100);
+            $table->string('slug', 100)->unique();
+            $table->string('arrival_station', 100);
+            $table->string('departure_station', 100);
+            $table->time('time_departure');
+            $table->time('time_arrival');
+            $table->string('train_code', 10);
+            $table->tinyInteger('carriage_number');
+            $table->boolean('in_time');
             $table->timestamps();
         });
     }
