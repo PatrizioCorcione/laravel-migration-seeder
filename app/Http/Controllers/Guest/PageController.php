d54c463cfc4937fb0951ea3ac652eea7936b723e
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-05-14 15:14:48 +0200

    [add] paginator whit table

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
-    public function index(){
-        return view('home');
+    public function index()
+    {
+        $trains = Train::paginate(10);
+        return view('home', compact('trains'));
     }
 
-    public function nuovaPagina(){
+    public function nuovaPagina()
+    {
         return view('nuova-pagina');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,27 @@
 @extends('layout.main')
 
 @section('content')
-
-<h1>Home</h1>
-
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $item)
+                <tr>
+                    <th>{{ $item->id }}</th>
+                    <td>{{ $item->slug }}</td>
+                    <td>{{ $item->company }}</td>
+                    <td>{{ $item->arrival_station }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <div class="container">
+        {{ $trains->links() }}
+    </div>
 @endsection
